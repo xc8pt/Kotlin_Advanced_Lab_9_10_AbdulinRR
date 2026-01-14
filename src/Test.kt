5d90ef0599d8b4cf7625dd20128b5e93b51ad261
gggg <gggg@gmail.com>
2026-01-14 11:25:16 +0300

    10

--- src/Test.kt
+++ src/Test.kt
@@ -3,10 +3,27 @@ var age: Int = 18
         if ((value > 0) and (value < 110))
             field = value
     }
+//fun main(){
+//    println(age)
+//    age = 45
+//    println(age)
+//    age = -345
+//    println(age)
+//}
+
+data class Item(
+    val id: Int,
+    val name: String,
+    val quantity: Int
+){
+    override fun toString(): String {
+        return "Id предмета: $id\nИмя: $name\nКоличество: $quantity\n"
+    }
+}
+
 fun main(){
-    println(age)
-    age = 45
-    println(age)
-    age = -345
-    println(age)
+    val sword = Item(1,"Sword",1)
+    val betterSword = sword.copy(quantity = 2)
+    println(sword.toString())
+    println(betterSword.toString())
 }
