3eadb4d93e3ef015220be8d00c2d4b8e739c0da5
xc8pt <brolike636@gmail.com>
2026-01-21 09:51:26 +0300

    Add interface

---
+++ src/Interface.kt
@@ -0,0 +1,69 @@
+interface Movable {
+    val model: String
+    val number: String
+    var speed: Int
+    fun move()
+    fun stop(){
+        println("Останавливаемся...")
+    }
+}
+class Car(override val model: String,
+          override val number: String)
+    : Movable {
+    override var speed = 60
+    override fun move(){
+        println("Едем на машине со скоростью $speed км/ч")
+    }
+}
+class Aircraft(override val model: String,
+               override val number: String)
+    : Movable{
+    override var speed = 600
+    override fun move(){
+        println("Летим на самолете со скоростью $speed")
+    }
+    override fun stop() = println("Приземлямся...")
+}
+fun travel (obj: Movable) = obj.move()
+
+interface Worker{
+    fun work()
+}
+interface Student{
+    fun study()
+}
+class WorkingStudents(val name: String) : Worker, Student{
+    override fun work() = println("$name работает")
+    override fun study() = println("$name учится")
+}
+
+interface VideoPlayable {
+    fun play() = println("Play video")
+}
+interface AudioPlayable {
+    fun play() = println("Play audio")
+}
+class MediaPlayer : VideoPlayable, AudioPlayable{
+    override fun play(){
+        println("Start playing")
+        super<VideoPlayable>.play()
+        super<AudioPlayable>.play()
+    }
+}
+fun main(){
+
+//    val car = Car("Lada", "Х006АМ134")
+//    val aircraft =  Aircraft("Boeing", "737")
+////    travel(car)
+////    travel(aircraft)
+//    val pavel = WorkingStudents("Pavel")
+//    pavel.work()
+//    pavel.study()
+//    car.move()
+//    car.stop()
+//    aircraft.move()
+//    aircraft.stop()
+    val player = MediaPlayer()
+    player.play()
+
+}
